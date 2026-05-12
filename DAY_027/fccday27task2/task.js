f4bc26dd519f83f890ab94af228c59e954b0d421
MORGUE28 <krubhere123@gmail.com>
2026-05-12 17:27:51 +0530

    done with day 27 of webdev commitment.

---
+++ DAY_027/fccday27task2/task.js
@@ -0,0 +1,21 @@
+console.log("Hello! I'm your coding fun fact guide!");
+
+let botName = "Trivia Bot";
+let botLocation = "Server 1";
+let favoriteLanguage = "JavaScript";
+
+console.log("My name is " + botName + " and I live on " + botLocation + ".");
+
+console.log("My favorite programming language is " + favoriteLanguage + ".");
+
+let codingFact = "Fun Fact: " + favoriteLanguage + " was famously created in just 10 days by Brendan Eich in May 1995.";
+console.log(codingFact);
+
+codingFact = "Fun Fact: " + favoriteLanguage + " is designed to be 100% backwards compatible, meaning code written in the 90s should still run today.";
+console.log(codingFact);
+
+codingFact = "Fun Fact: " + favoriteLanguage + " was created to interact quickly with DOM elements and make animations, complementing the functionalities provided by the Java language.";
+console.log(codingFact);
+
+console.log("It was fun sharing these facts with you. Goodbye! - " + botName + " from " + botLocation + ".");
+
---
+++ DAY_027/fccday27task3/task.js
@@ -0,0 +1,21 @@
+let adjective ="hungry";
+let noun ="lion";
+let verb ="hungry";
+let place ="den";
+let adjective2 ="big";
+let noun2 ="lion";
+
+let firstStory = "Once upon a time, there was a(n) " + adjective + " " + noun + " who loved to eat " + noun2 + ". The " + noun + " lived in a " + place + " and had " + adjective2 + " nostrils that blew fire when it was " + verb + ".";
+
+console.log("First story: "+ firstStory)
+
+adjective ="strong";
+noun ="dragon";
+verb ="angry";
+place ="den";
+adjective2 ="small";
+noun2 ="dragon";
+
+let secondStory ="Once upon a time, there was a(n) " + adjective + " " + noun + " who loved to eat " + noun2 + ". The " + noun + " lived in a " + place + " and had " + adjective2 + " nostrils that blew fire when it was " + verb + ".";
+
+console.log("Second story: "+ secondStory)
